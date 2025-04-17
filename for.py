70f67dca0d8f1ae69993640bf60d98cf2bf81369
Mopzito <itzmopz91@gmail.com>
2025-04-17 12:54:13 -0400

    ejercicos for

---
+++ for.py
@@ -0,0 +1,29 @@
+# for i in range(1,11):
+#  print("repeticion numero", i)
+
+# num=int(input("ingrese un numero"))
+# for i in range (1,11):
+#     print(num, "x",i, "=", num*i)
+
+# for i in range(10,0,-1):
+#     print(i )
+
+# pida al usuario 3 notas y saque promedio
+# uso de for obligatorio
+
+
+# cant=int(input("ingrese la cantidad de notas "))
+
+# suma=0
+# for i in range (cant): 
+#     n1=int(input("ingrese la nota "))
+#     suma=suma+n1
+# prom=suma/cant
+# print("el promedio es", round(prom))
+
+# cant=int(input("ingrese la cantidad de numeros "))
+# suma=0
+# for i in range (cant):
+#     n1=int(input("ingrese numeros "))
+#     suma+=n1
+# print("la suma de todos los numeros es ", suma)
